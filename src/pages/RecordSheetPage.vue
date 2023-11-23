8d701d7582dd2a1233ddea5612dcfc8ce66065f6
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2023-11-23 15:28:18 +0530

    add small animation when adding/removing items

--- src/pages/RecordSheetPage.vue
+++ src/pages/RecordSheetPage.vue
@@ -46,23 +46,27 @@
     <q-tab-panels swipeable v-model="record_tab">
       <q-tab-panel name="expenses" class="q-pa-none">
         <q-list>
-          <record-item
-            v-for="record in expense_records"
-            :key="record.id"
-            :record="record"
-            @edit="open_edit_record"
-          />
+          <TransitionGroup name="fade">
+            <record-item
+              v-for="record in expense_records"
+              :key="record.id"
+              :record="record"
+              @edit="open_edit_record"
+            />
+          </TransitionGroup>
         </q-list>
       </q-tab-panel>
 
       <q-tab-panel name="income" class="q-pa-none">
         <q-list>
-          <record-item
-            v-for="record in income_records"
-            :key="record.id"
-            :record="record"
-            @edit="open_edit_record"
-          />
+          <TransitionGroup name="fade">
+            <record-item
+              v-for="record in income_records"
+              :key="record.id"
+              :record="record"
+              @edit="open_edit_record"
+            />
+          </TransitionGroup>
         </q-list>
       </q-tab-panel>
     </q-tab-panels>
@@ -202,3 +206,25 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+/* 1. declare transition */
+.fade-move,
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+/* 2. declare enter from and leave to state */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scaleY(0.01) translate(30px, 0);
+}
+
+/* 3. ensure leaving items are taken out of layout flow so that moving
+      animations can be calculated correctly. */
+.fade-leave-active {
+  position: absolute;
+}
+</style>
